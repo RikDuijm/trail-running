cc8d004d7ca93f74b902b9c757b3d26e002d9466
RikDuijm <rikduijm@gmail.com>
2020-02-04 13:35:45 +0000

    recreacted add new post funcationality

--- community/models.py
+++ community/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     """
     A single Blog post
     """
+    author = models.ForeignKey(User, null=False, default=1, on_delete=models.SET_DEFAULT)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
--- community/views.py
+++ community/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import BlogPostForm
+from django.http import HttpResponseForbidden
+from django.contrib import messages
 
 def get_posts(request):
     """
@@ -12,6 +14,7 @@ def get_posts(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()
         ).order_by('-published_date')
     return render(request, "blogposts.html", {'posts': posts})
+
 
 def post_detail(request, pk):
     """
@@ -26,18 +29,23 @@ def post_detail(request, pk):
     post.save()
     return render(request, "postdetail.html", {'post': post})
 
-def create_or_edit_post(request, pk=None):
+def new_post(request, pk=None):
     """
-    Create a view that allows us to create
-    or edit a post depending if the Post ID
-    is null or not
+    Create a view that allows user to add new post
     """
     post = get_object_or_404(Post, pk=pk) if pk else None
-    if request.method == "POST":
-        form = BlogPostForm(request.POST, request.FILES, instance=post)
-        if form.is_valid():
-            post = form.save()
-            return redirect(post_detail, post.pk)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = BlogPostForm(request.POST, request.FILES)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return redirect('get_posts')
+        else:
+            form = BlogPostForm()
     else:
-        form = BlogPostForm(instance=post)
+        return redirect('login')        
     return render(request, 'blogpostform.html', {'form': form})
+ 
+ 
