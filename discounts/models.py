7c0a4d6cde0685fa8e94fa12a21af4c4c551257c
RikDuijm <rikduijm@gmail.com>
2020-02-11 13:07:10 +0000

    expanded discounts model and updated css

--- discounts/models.py
+++ discounts/models.py
@@ -5,7 +5,11 @@ import datetime
 # Create your models here.
 class Product(models.Model):
     product_name = models.CharField(max_length=254, default='')
-    description = models.TextField()
+    description_introduction = models.TextField(null=True)
+    description_first = models.TextField(null=True,)
+    description_second = models.TextField(null=True,)
+    description_third = models.TextField(null=True, blank=True)
+    normal_price = models.DecimalField(max_digits=6, decimal_places=2, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True,
@@ -13,7 +17,7 @@ class Product(models.Model):
     valid_until = models.DateTimeField(blank=True, null=True,
                                           default=timezone.now)                                     
     views = models.IntegerField(default=0)
-    image = models.ImageField(upload_to="images", blank=True, null=True)
+    image = models.ImageField(upload_to="product_images", blank=True, null=True)
 
 
     def __str__(self):
